4bc33617c0110e6b788885516219dcc936f45fc9
Bodó Dávid <h848002@stud.u-szeged.hu>
2020-10-20 23:38:39 +0200

    added in the end and csepereg az eso to songs

--- songs.js
+++ songs.js
@@ -18,18 +18,6 @@ module.exports = {
     "We know the game and we're gonna play it\n" +
     "And if you ask me how I'm feeling\n" +
     "Don't tell me you're too blind to see\n" +
-    "Never gonna give you up\n" +
-    "Never gonna let you down\n" +
-    "Never gonna run around and desert you\n" +
-    "Never gonna make you cry\n" +
-    "Never gonna say goodbye\n" +
-    "Never gonna tell a lie and hurt you\n" +
-    "Never gonna give you up\n" +
-    "Never gonna let you down\n" +
-    "Never gonna run around and desert you\n" +
-    "Never gonna make you cry\n" +
-    "Never gonna say goodbye\n" +
-    "Never gonna tell a lie and hurt you\n" +
     "Never gonna give never gonna give\n" +
     "Give you up\n" +
     "Ooh Never gonna give never gonna give\n" +
@@ -40,22 +28,6 @@ module.exports = {
     "We know the game and we're gonna play it\n" +
     "I just wanna tell you how I'm feeling\n" +
     "Gotta make you understand\n" +
-    "Never gonna give you up\n" +
-    "Never gonna let you down\n" +
-    "Never gonna run around and desert you\n" +
-    "Never gonna make you cry\n" +
-    "Never gonna say goodbye\n" +
-    "Never gonna tell a lie and hurt you\n" +
-    "Never gonna give you up\n" +
-    "Never gonna let you down\n" +
-    "Never gonna run around and desert you\n" +
-    "Never gonna make you cry\n" +
-    "Never gonna say goodbye\n" +
-    "Never gonna tell a lie and hurt you\n" +
-    "Never gonna give you up\n" +
-    "Never gonna let you down\n" +
-    "Never gonna run around and desert you\n" +
-    "Never gonna make you cry \n" +
     "Mama just killed a man\n" +
         "Put a gun against his head\n" +
         "Pulled my trigger now he's dead\n" +
@@ -71,7 +43,95 @@ module.exports = {
         "Body's aching all the time\n" +
         "Goodbye everybody I've got to go\n" +
         "Gotta leave you all behind and face the truth\n" +
-        "Mama ooh\n" +
+        "Mama oooh\n" +
         "I don't want to die\n" +
-        "I sometimes wish I'd never been born at all"
+        "I sometimes wish I'd never been born at all\n" +
+    "It starts with one thing\n" +
+    "I don't know why\n" +
+    "It doesn't even matter how hard you try\n" +
+    "Keep that in mind\n" +
+    "I designed this rhyme\n" +
+    "To explain in due time\n" +
+    "All I know\n" +
+    "Time is a valuable thing\n" +
+    "Watch it fly by as the pendulum swings\n" +
+    "Watch it count down to the end of the day\n" +
+    "The clock ticks life away\n" +
+    "It's so unreal\n" +
+    "Didn't look out below\n" +
+    "Watch the time go right out the window\n" +
+    "Trying to hold on but you didn't even know\n" +
+    "Wasted it all just to watch you go\n" +
+    "I kept everything inside\n" +
+    "And even though I tried it all fell apart\n" +
+    "What it meant to me\n" +
+    "Will eventually be a memory of a time when\n" +
+    "I tried so hard\n" +
+    "And got so far\n" +
+    "But in the end\n" +
+    "It doesn't even matter\n" +
+    "I had to fall\n" +
+    "To lose it all\n" +
+    "But in the end\n" +
+    "It doesn't even matter\n" +
+    "One thing I don't know why\n" +
+    "It doesn't even matter how hard you try\n" +
+    "Keep that in mind\n" +
+    "I designed this rhyme\n" +
+    "To remind myself of a time when\n" +
+    "I tried so hard\n" +
+    "In spite of the way you were mocking me\n" +
+    "Acting like I was part of your property\n" +
+    "Remembering all the times you fought with me\n" +
+    "I'm surprised it got so\n" +
+    "Things aren't the way they were before\n" +
+    "You wouldn't even recognize me anymore\n" +
+    "Not that you knew me back then\n" +
+    "But it all comes back to me in the end\n" +
+    "You kept everything inside\n" +
+    "And even though I tried it all fell apart\n" +
+    "What it meant to me will eventually be a memory of a time when\n" +
+    "I tried so hard\n" +
+    "And got so far\n" +
+    "But in the end\n" +
+    "It doesn't even matter\n" +
+    "I had to fall\n" +
+    "To lose it all\n" +
+    "But in the end\n" +
+    "It doesn't even matter\n" +
+    "I've put my trust in you\n" +
+    "Pushed as far as I can go\n" +
+    "For all this\n" +
+    "There's only one thing you should know\n" +
+    "I've put my trust in you\n" +
+    "Pushed as far as I can go\n" +
+    "For all this\n" +
+    "There's only one thing you should know\n" +
+    "I tried so hard\n" +
+    "And got so far\n" +
+    "But in the end\n" +
+    "It doesn't even matter\n" +
+    "I had to fall\n" +
+    "To lose it all\n" +
+    "But in the end\n" +
+    "It doesn't even matter\n" +
+        "Csepereg az eső\n" +
+    "lefolyik az arcomon\n" +
+    "Régi barátok\n" +
+    "veletek vagyok\n" +
+    "Már nagyon nagyon rég\n" +
+    "jóban rosszban\n" +
+    "együtt vagyunk\n" +
+        "Nem feledünk mi\n" +
+    "barátok maradunk\n" +
+        "Eshet az eső\n" +
+    "fújhat a szél\n" +
+    "A szívünket\n" +
+    "nem törte meg\n" +
+    "semmi még\n" +
+        "Néha volt vitánk\n" +
+    "de szóval se\n" +
+    "bántjuk egymást\n" +
+    "Bármi legyen is\n" +
+    "együtt van a 4 barát\n"
 }
