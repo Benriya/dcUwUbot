b82cdf5a670bad353f27596a4b5eeddc0b4952d3
Bodó Dávid <h848002@stud.u-szeged.hu>
2021-05-25 15:31:10 +0200

    updated !help command

--- functions.js
+++ functions.js
@@ -143,6 +143,8 @@ export default {
                     '`!unsub + "teszttel rendelkező kurzus":` Leiratkozol a pingekről\n\n' +
                     '`!teszt + "kurzus":` Akik fel vannak iratkozva a kurzusra azokat megpingelem\n\n' +
                     '`!risus + "channel név":` Tipik risusing csak te nem használhatod ezt'},
+            { Parancsok: '`!bullshit:` Ez bikaszar\n\n' +
+                    '`!punch:` Megcsaplak\n\n' },
             { Parancsok: '`"bully"` a szövegben, akkor lespriccelem a másikat.\n\n' +
                     '`"no bully"` a szövegben azt eredményezi hogy egy stop képet küldök, az abuse megszüntetésére.\n\n' +
                     '`Ha 3x beküldik ugyanazt, akkor megismétlem`\n\n' +
